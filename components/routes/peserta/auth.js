36fd7a1d2a418eae9fa9a43e12ca062437b2d5f0
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-09-03 08:54:25 +0700

    add API to get profile

--- components/routes/peserta/auth.js
+++ components/routes/peserta/auth.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { authMiddleware } = require("../../../src/middleware/auth");
-const { loginPeserta, registerPeserta } = require("../../controllers/auth");
+const {
+  loginPeserta,
+  registerPeserta,
+  profile,
+} = require("../../controllers/auth");
 
 router.post("/login", loginPeserta);
 router.post("/register", registerPeserta);
+router.get("/profile", authMiddleware(["peserta"]), profile);
 
 module.exports = router;
